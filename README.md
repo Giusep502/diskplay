ba3dd8eb30d8c4f80d95c78f037e89e6d30e73fe
Giuseppe Di Francesco <giuseppedifrancesco@unobravo.com>
2025-02-01 13:05:51 +0100

    Init App

---
+++ README.md
@@ -0,0 +1,3 @@
+# diskplay_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/diskplay_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.diskplay_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
